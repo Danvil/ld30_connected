0a01a67fcaddaddc5fd00b1efe94be988db4f1b0
David W <davidw@danvil.de>
2014-08-25 17:48:48 +0200

    Better and random galaxy generation.

--- Assets/Galaxy.cs
+++ Assets/Galaxy.cs
@@ -54,19 +54,31 @@ public class Galaxy : MonoBehaviour {
 		Singleton = this;
 	}
 
-	// Use this for initialization
-	void Start () {
-	
+	WorldGroup CreateWorld(Vector3 pos)
+	{
+		GameObject go = (GameObject)Instantiate(pfWorld);
+		go.transform.position = pos;
+		WorldGroup wg = go.GetComponent<WorldGroup>();
+		wg.Team = Team.NEUTRAL;
+		AddWorld(wg);
+		return wg;
+	}
+
+	void GenerateGalaxyOne()
+	{
+		CreateWorld(Vector3.zero);
+		worlds[0].World.initRobotTeam = Globals.Singleton.playerTeam;		
+	}
+
+	void GenerateGalaxySimple()
+	{
 		const float SPACE = 80.0f;
 		const float RND = 0.2f;
 
 		for(int y=0; y<2; y++) {
 			for(int x=0; x<2; x++) {
-				GameObject go = (GameObject)Instantiate(pfWorld);
-				go.transform.position = new Vector3(SPACE*x,0,SPACE*y) + SPACE * RND * new Vector3(Tools.Random(-1f,+1f),0,Tools.Random(-1f,+1f));
-				WorldGroup wg = go.GetComponent<WorldGroup>();
-				wg.Team = Team.NEUTRAL;
-				AddWorld(wg);
+				Vector3 pos = new Vector3(SPACE*x,0,SPACE*y) + SPACE * RND * new Vector3(Tools.Random(-1f,+1f),0,Tools.Random(-1f,+1f));
+				CreateWorld(pos);
 			}
 		}
 
@@ -78,6 +90,68 @@ public class Galaxy : MonoBehaviour {
 		AddConnection(worlds[3], worlds[1]);
 		AddConnection(worlds[3], worlds[2]);
 
+	}
+
+	WorldGroup TryPlace(List<WorldGroup> open, float size, float rmin, float rmax)
+	{
+		float sizefudge = size;
+		while(true) {
+			var wg = open.RandomSample();
+			float r = Tools.Random(rmin, rmax);
+			float a = Tools.Random(0, 2.0f*Mathf.PI);
+			Vector3 p = wg.transform.position + r * new Vector3(Mathf.Cos(a), 0, Mathf.Sin(a));
+			if(0 <= p.x && p.x <= sizefudge && 0 <= p.z && p.z <= sizefudge) {
+				if(!worlds.Any(x => (x.transform.position - p).magnitude < rmin)) {
+					return CreateWorld(p);
+				}
+			}
+			sizefudge += 0.05f*size;
+		}
+	}
+
+	void GenerateGalaxyOrganic()
+	{
+		const int NUM = 2;
+		const float SIZE = 130.0f;
+		const float R_MIN = 55.0f;
+		const float R_MAX = 90.0f;
+
+
+		Vector3 pos1 = Vector3.zero;
+		Vector3 pos2 = new Vector3(SIZE,0,SIZE);
+
+		// initial worlds
+		CreateWorld(pos1);
+		worlds[0].World.initRobotTeam = Globals.Singleton.playerTeam;
+		CreateWorld(pos2);
+		worlds[1].World.initRobotTeam = Globals.Singleton.nonPlayerTeam;
+
+		// grow galaxy
+		List<WorldGroup> open1 = new List<WorldGroup>();
+		open1.Add(worlds[0]);
+		List<WorldGroup> open2 = new List<WorldGroup>();
+		open2.Add(worlds[1]);
+		for(int i=0; i<NUM; i++) {
+			open1.Add(TryPlace(open1, SIZE, R_MIN, R_MAX));
+			open2.Add(TryPlace(open2, SIZE, R_MIN, R_MAX));
+		}
+
+		// connections
+		for(int i=0; i<worlds.Count; i++) {
+			var a = worlds[i];
+			for(int j=i+1; j<worlds.Count; j++) {
+				var b = worlds[j];
+				float d = (a.transform.position - b.transform.position).magnitude;
+				if(d <= R_MAX) {
+					AddConnection(a, b);
+				}
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		GenerateGalaxyOrganic();
 	}
 	
 	// Update is called once per frame

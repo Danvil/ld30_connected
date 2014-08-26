8d5817ac47df0da3ee117167bc73b8b66c6dcfa8
David W <davidw@danvil.de>
2014-08-26 16:57:57 +0200

    Bugfix: Fixed camera scrolling. 'H' now centers on home world. 'G' now centers on galaxy center.

--- Assets/WorldSelector.cs
+++ Assets/WorldSelector.cs
@@ -23,6 +23,11 @@ public class WorldSelector : MonoBehaviour {
 	public float scrollRate = 10.0f;
 
 	float height;
+
+	float OneOverTanAlpha
+	{
+		get { return 1.0f / Mathf.Tan(Mathf.Deg2Rad * camera.transform.rotation.eulerAngles.x); }
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -32,16 +37,22 @@ public class WorldSelector : MonoBehaviour {
 			float heightold = height;
 			height -= Time.deltaTime * scrollRate * v;
 			height = Mathf.Min(Mathf.Max(heightLow,height), heightHigh);
-			float x = camera.transform.position.x;
-			float z = camera.transform.position.z * height / heightold;
-			camera.transform.position = new Vector3(x,height,z);
+			float dz = (heightold - height) * OneOverTanAlpha;
+			camera.transform.position = new Vector3(camera.transform.position.x,height,camera.transform.position.z+dz);
+		}
+		// center
+		if(Input.GetKeyDown(KeyCode.G)) {
+			Vector3 mp = Galaxy.Singleton.WorldsMeanPoint();
+			height = heightHigh;
+			float z = height * OneOverTanAlpha;
+			camera.transform.position = mp + new Vector3(0,height,-z);
 		}
 		// home
 		if(Input.GetKeyDown(KeyCode.H)) {
-			Vector3 mp = Galaxy.Singleton.WorldsMeanPoint();
-			float z = heightHigh / Mathf.Tan(Mathf.Deg2Rad * camera.transform.rotation.eulerAngles.x);
-			camera.transform.position = mp + new Vector3(0,heightHigh,-z);
+			Vector3 mp = Vector3.zero; // TODO get home world coordinates
 			height = heightHigh;
+			float z = height * OneOverTanAlpha;
+			camera.transform.position = mp + new Vector3(0,height,-z);
 		}
 	}
 }

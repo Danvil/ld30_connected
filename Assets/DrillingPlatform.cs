45fc3697860555d1d2102405efe8fa3fdad6d264
David W <davidw@danvil.de>
2014-08-26 18:17:35 +0200

    Minor: Driller now produces ressources. Robots now cost goo.

--- Assets/DrillingPlatform.cs
+++ Assets/DrillingPlatform.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class DrillingPlatform : MonoBehaviour {
 
+	public float mineralsRate = 0.25f;
+	public float gooRate = 0.15f;
+
+	World world;
+
 	// Use this for initialization
 	void Start () {
-	
+		world = this.GetComponent<Entity>().world;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-	void OnDrawGizmos()
-	{
+		Team team = world.WorldGroup.Team;
+		GlobalInterface.Singleton.GetTeamRessources(team).numMinerals += Time.deltaTime * mineralsRate;
+		GlobalInterface.Singleton.GetTeamRessources(team).numGoo += Time.deltaTime * gooRate;
 	}
 }
